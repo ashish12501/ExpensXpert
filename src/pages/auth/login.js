c1cd8d8b6aaa2bd8c0da056d9f4c22c8b8d389a1
ashish12501 <ay079396@gmail.com>
2023-10-13 14:50:17 +0530

    added responsoveness to login page

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -5,6 +5,9 @@ import Google from '../../images/google.png'
 import User from '../../images/user.png'
 import { signInAnonymously } from 'firebase/auth'
 import { auth, provider, fbProvider } from '../../config/firebase-config';
+import { Footer } from '../../components/footer'
+
+
 
 import { signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom';
@@ -34,18 +37,21 @@ export function Login() {
     };
 
     return (
-        <div className="registerpage">
-            <div className="registerform">
-                <h1>Welcome to ExpensXpert!</h1>
-                <p className='continue'>Signin to Continue</p>
-                <div className='loginButtons' >
-                    <button className='google' onClick={authenticateUsingGoogle}><img alt="" src={Google} /> Continue with Google </button>
-                    <button className='facebook' onClick={authenticateUsingFacebook}><img alt="" src={Facebook} />Continue with Facebook</button>
-                    <p className='or'>Or</p>
-                    <button className='guest' onClick={authenticateAsGuest}><img alt="" src={User} />Continue as Guest</button>
+        <>
+            <div className="registerpage">
+                <div className="registerform">
+                    <h1>Welcome to ExpensXpert!</h1>
+                    <p className='continue'>Signin to Continue</p>
+                    <div className='loginButtons' >
+                        <button className='google' onClick={authenticateUsingGoogle}><img alt="" src={Google} /> Continue with Google </button>
+                        <button className='facebook' onClick={authenticateUsingFacebook}><img alt="" src={Facebook} />Continue with Facebook</button>
+                        <p className='or'>Or</p>
+                        <button className='guest' onClick={authenticateAsGuest}><img alt="" src={User} />Continue as Guest</button>
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
