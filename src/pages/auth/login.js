9e40db93868b99e1a56dbc897611886fa049c73e
ashish12501 <ay079396@gmail.com>
2023-10-15 21:06:16 +0530

    buttons transitions

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
 import Facebook from '../../images/facebook.png'
 import Google from '../../images/google.png'
@@ -6,27 +6,50 @@ import User from '../../images/user.png'
 import { signInAnonymously } from 'firebase/auth'
 import { auth, provider, fbProvider } from '../../config/firebase-config';
 import { Footer } from '../../components/footer'
-
-
 import { signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom';
+
+
 export function Login() {
+
+    const [buttonClick1, setButtonClick1] = useState(false)
+    const [buttonClick2, setButtonClick2] = useState(false)
+    const [buttonClick3, setButtonClick3] = useState(false)
+
     const navigate = useNavigate();
     const authenticateUsingGoogle = async () => {
-        const result = await signInWithPopup(auth, provider);
-        console.log(result);
-        navigate('/tracker')
+        try {
+            setButtonClick1(true);
+            setButtonClick2(false);
+            setButtonClick3(false);
+
+            const result = await signInWithPopup(auth, provider);
+            console.log(result);
+            navigate('/tracker')
+        } catch (error) {
+            console.error("Authentication failed:", error);
+        }
     };
 
     const authenticateUsingFacebook = async () => {
-        const result = await signInWithPopup(auth, fbProvider);
-        console.log(result);
-        navigate('/tracker')
+        try {
+            setButtonClick2(true);
+            setButtonClick1(false);
+            setButtonClick3(false);
+            const result = await signInWithPopup(auth, fbProvider);
+            console.log(result);
+            navigate('/tracker')
+        } catch (error) {
+            console.error("Authentication failed:", error);
+        }
     };
 
 
     const authenticateAsGuest = async () => {
         try {
+            setButtonClick3(true);
+            setButtonClick2(false);
+            setButtonClick1(false);
             const result = await signInAnonymously(auth);
             console.log(result);
             navigate('/tracker');
@@ -42,10 +65,10 @@ export function Login() {
                     <h1>Welcome to ExpensXpert!</h1>
                     <p className='continue'>Signin to Continue</p>
                     <div className='loginButtons' >
-                        <button className='google' onClick={authenticateUsingGoogle}><img alt="" src={Google} /> Continue with Google </button>
-                        <button className='facebook' onClick={authenticateUsingFacebook}><img alt="" src={Facebook} />Continue with Facebook</button>
+                        <button className={buttonClick1 ? "active" : "google"} onClick={authenticateUsingGoogle}><img alt="" src={Google} /> Continue with Google </button>
+                        <button className={(buttonClick2 ? "factive" : "facebook")} onClick={authenticateUsingFacebook}><img alt="" src={Facebook} />Continue with Facebook</button>
                         <p className='or'>Or</p>
-                        <button className='guest' onClick={authenticateAsGuest}><img alt="" src={User} />Continue as Guest</button>
+                        <button className={buttonClick3 ? "active" : "guest"} onClick={authenticateAsGuest}><img alt="" src={User} />Continue as Guest</button>
                     </div>
                 </div>
             </div>
